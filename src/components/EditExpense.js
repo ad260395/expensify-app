d1a1c2531fc897ba69ed3b32fe51ceb6dd9bf71f
Ashish Deshpande <ashish.deshpande@ad.infosys.com>
2021-05-26 14:01:28 +0530

    Update Expenses functionality added

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,13 +1,13 @@
 import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
-import { editExpense, startRemoveExpense } from "../actions/expenses";
+import { startEditExpense, startRemoveExpense } from "../actions/expenses";
 const EditExpense = (props) => {
 	return (
 		<div>
 			<ExpenseForm
 				expense={props.expense}
 				onSubmit={(expense) => {
-					props.dispatch(editExpense(props.expense.id, expense));
+					props.dispatch(startEditExpense(props.expense.id, expense));
 					props.history.push("/");
 				}}
 			/>
@@ -31,7 +31,7 @@ const mapStateToProps = (state, props) => {
 };
 
 const mapDispatchToProps = (dispatch, props) => ({
-	editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+	editExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
 	startRemoveExpense: (data) => dispatch(startRemoveExpense(data)),
 	dispatch,
 });
